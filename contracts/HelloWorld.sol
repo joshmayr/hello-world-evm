5e48b1b3d13d3e0afec47641275084bf2b9e0fb1
joshmayr <joshmayr@umich.edu>
2022-09-18 15:23:08 -0400

    Initial commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract HelloWorld {
+    struct Message {
+        address sender;
+        string message;
+        uint256 timestamp;
+    }
+
+    event MessageSent(address sender, string message, uint256 timestamp);
+
+    Message[] messages;
+    mapping(address => uint256) lastMessage;
+    uint256 totalMessages;
+
+    constructor() {
+        sendMessage("Hello, World!");
+    }
+
+    function sendMessage(string memory _message) public {
+        messages.push(Message(msg.sender, _message, block.timestamp));
+        lastMessage[msg.sender] = totalMessages;
+        totalMessages += 1;
+        emit MessageSent(msg.sender, _message, block.timestamp);
+    }
+
+    function getAllMessages() public view returns (Message[] memory) {
+        return messages;
+    }
+    function getLastMessageFromAddress(address _sender) public view returns (Message memory) {
+        require(lastMessage[_sender] > 0, "User hasn't sent any messages");
+        return messages[lastMessage[_sender]];
+    }
+}
+
+// TODO: Write tests for this
---
+++ test/HelloWorld.ts
@@ -0,0 +1,49 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+
+describe("HelloWorld", function () {
+  async function deployHelloWorldFixture() {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const helloWorld = await HelloWorld.deploy();
+
+    return { helloWorld, owner, otherAccount };
+  }
+
+  describe("Send message", function () {
+    it("Should emit MessageSent event", async function () {
+      const { helloWorld, owner } = await loadFixture(deployHelloWorldFixture);
+      await expect(helloWorld.sendMessage("Hello, World!"))
+        .to.emit(helloWorld, "MessageSent")
+        .withArgs(await owner.address, "Hello, World!", anyValue);
+    });
+  });
+
+  describe("Read messages", function () {
+    it("GetAllMessages should return all messages", async function () {
+      const { helloWorld } = await loadFixture(deployHelloWorldFixture);
+      expect((await helloWorld.getAllMessages()).length).to.equal(1);
+      const tx = await helloWorld.sendMessage("Second message");
+      expect((await helloWorld.getAllMessages()).length).to.equal(2);
+    });
+
+    it("GetLastMessageFromAddress should print user's last message", async function () {
+        const { helloWorld, owner, otherAccount } = await loadFixture(deployHelloWorldFixture);
+        await helloWorld.sendMessage("Hello, World!");
+        expect((await helloWorld.getLastMessageFromAddress(owner.address)).message).to.equal("Hello, World!");
+        await helloWorld.sendMessage("Second message");
+        expect((await helloWorld.getLastMessageFromAddress(owner.address)).message).to.equal("Second message");
+    });
+
+    it("GetLastMessageFromAddress should revert when user hasn't sent any messages", async function () {
+      const { helloWorld, owner, otherAccount } = await loadFixture(deployHelloWorldFixture);
+      await helloWorld.sendMessage("Hello, World!");
+      await expect(helloWorld.getLastMessageFromAddress(otherAccount.address)).to.be.revertedWith(
+        "User hasn't sent any messages"
+      );
+    });
+  });
+});
